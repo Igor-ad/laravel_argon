6f9d35f13a4d9e9091e7304dbf9a0e8074cd1c19
Igor-ad <autodoctor.odessa@gmail.com>
2021-03-03 14:36:48 +0200

    traking stat added

---
+++ database/migrations/2021_02_27_091636_add_code_and_chat_id_to_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCodeAndChatIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('chat_id')
+                ->unique()
+                ->nullable()
+                ->default(null)
+                ->after('api_token');
+
+            $table->string('code')
+                ->unique()
+                ->nullable()
+                ->default(null)
+                ->after('chat_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('chat_id');
+            $table->dropColumn('code');
+        });
+    }
+}
---
+++ database/migrations/2021_02_28_064115_create_tracking_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrackingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trackings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable(false);
+            $table->string('user_agent')->default(null);
+            $table->string('ip')->default(null);
+            $table->string('country')->default(null);
+            $table->string('product')->default(null);
+            $table->integer('counter')->default(null);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracking');
+    }
+}
